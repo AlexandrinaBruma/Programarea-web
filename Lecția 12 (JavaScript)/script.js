902e79678db1567acf8a25223325ce31c1e21914
Bruma Alexandrina <alexandrinabruma04@gmail.com>
2026-04-27 10:27:04 +0300

    Add new page

---
+++ Lecția 12 (JavaScript)/script.js
@@ -0,0 +1,29 @@
+console.log("Bruma Alexandrina");
+
+let a = 2;
+let b = 1.7;
+let c = Infinity;
+let d = "bella" - 3;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d); //NaN - Not a Number
+
+let nume = "Alexandrina";
+let hello = `Buna ziua, ${nume}`;
+console.log(hello);
+
+let v = "CEITI e colegiu";
+console.log(v.length);
+console.log(v + " in Chisinau");
+console.log(v.indexOf("CEITI"));
+console.log(v.substring(0, 5));
+
+let x = "\"CEITI\" este colegiu";
+console.log(x);
+
+let A = "Ion este" + " medic";
+console.log(A);
+
+console.log(NaN == NaN);
+console.log(typeof(NaN));
---
+++ Studiu Individual/script.js
@@ -0,0 +1,23 @@
+function backToHome() {
+    window.open("index.html");
+}
+
+function goToLogin() {
+    window.open("adminLogin.html");
+}
+
+function goToAdmin() {
+    window.open("adminPage.html");
+}
+
+function editPopUpOpen() {
+    window.open("popUp.html");
+}
+
+function addProduct() {
+    window.open("addProduct.html");
+}
+
+function save() {
+    window.close();
+}
